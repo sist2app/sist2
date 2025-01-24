5a82581992a34955f225bb2c0e61839ca2b5ce6b
Shy <me@shyy.io>
2025-01-23 21:40:27 -0500

    Fix magic database problem

--- scripts/magic_static.py
+++ scripts/magic_static.py
@@ -1,5 +1,16 @@
 
-with open("/usr/lib/file/magic.mgc", "rb") as f:
-    data = f.read()
+MAGIC_PATHS = [
+    "/vcpkg/installed/x64-linux/share/libmagic/misc/magic.mgc",
+    "/work/vcpkg/installed/x64-linux/share/libmagic/misc/magic.mgc",
+    "/usr/lib/file/magic.mgc"
+]
+
+for path in MAGIC_PATHS:
+    try:
+        with open(path, "rb") as f:
+            data = f.read()
+        break
+    except:
+        continue
 
 print("char magic_database_buffer[%d] = {%s};" % (len(data), ",".join(str(int(b)) for b in data)))
