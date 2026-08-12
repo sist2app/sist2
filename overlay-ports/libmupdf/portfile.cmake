b420f7adc5ed82b093735f0f558de146c593e755
Shy <me@shyy.io>
2026-08-12 16:23:47 -0400

    Patch mupdf to bound text objects only when something reads the box

--- overlay-ports/libmupdf/portfile.cmake
+++ overlay-ports/libmupdf/portfile.cmake
@@ -10,6 +10,7 @@ vcpkg_from_github(
     HEAD_REF master
     PATCHES
         0001-cache-glyph-names-in-simple-font-encoding.patch
+        0002-bound-text-objects-lazily.patch
 )
 file(COPY "${CMAKE_CURRENT_LIST_DIR}/CMakeLists.txt" DESTINATION "${SOURCE_PATH}")
 file(COPY "${CMAKE_CURRENT_LIST_DIR}/unofficial-libmupdf-config.cmake.in" DESTINATION "${SOURCE_PATH}")
